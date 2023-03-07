c133424418fa974f67bf1cd784bac926bec14a7c
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2023-03-07 10:40:53 +0530

    Update String_Method_Question.py

--- Udacity-Python_NanoDegree/Introduction_to_Python_Programming/String_Method_Question.py
+++ Udacity-Python_NanoDegree/Introduction_to_Python_Programming/String_Method_Question.py
@@ -7,10 +7,10 @@ What is the count of occurrences of the word 'you' in the verse? """
 
 verse = "If you can keep your head when all about you\n  Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n  But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n  Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n  And yet don’t look too good, nor talk too wise:"
 print(verse)
-print(len(verse))
+print(len(verse)) #362
 # Use the appropriate functions and methods to answer the questions above
 # Bonus: practice using .format() to output your answers in descriptive messages!
-print(verse.count("you"))
-print((verse.find("and")))
-print((verse.rfind("you")))
+print(verse.count("you")) #8
+print((verse.find("and"))) #65
+print((verse.rfind("you"))) #186
 
