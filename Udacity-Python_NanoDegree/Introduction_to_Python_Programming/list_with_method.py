16396bd8bcecbdfa567228cfc445e336c9c4cd6e
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2023-03-07 11:08:39 +0530

    Create list_with_method.py

---
+++ Udacity-Python_NanoDegree/Introduction_to_Python_Programming/list_with_method.py
@@ -0,0 +1,10 @@
+# Data structures are containers that organize and group data types togetherin different ways. A list is one of the most common and basic data structures in python.
+# List = A List is data structure in Python that is a mutable, ordered, sequence of elements,
+#Example => list_of_random_things = [1, 3.4, 'a string', True]
+#Example 2
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+print(months[0]) # January
+print(months[1]) # February
+print(months[7]) # August
+print(months[-1]) # December
+print(months[25]) # IndexError: list index out of range
