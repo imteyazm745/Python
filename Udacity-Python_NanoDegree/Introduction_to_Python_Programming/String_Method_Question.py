b220ffc7132a028309da025e570688c4d81ac0c0
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2023-03-07 10:46:19 +0530

    Update String_Method_Question.py

--- Udacity-Python_NanoDegree/Introduction_to_Python_Programming/String_Method_Question.py
+++ Udacity-Python_NanoDegree/Introduction_to_Python_Programming/String_Method_Question.py
@@ -14,3 +14,11 @@ print(verse.count("you")) #8
 print((verse.find("and"))) #65
 print((verse.rfind("you"))) #186
 
+# solution 2
+verse = "If you can keep your head when all about you\n  Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n  But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n  Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n  And yet don’t look too good, nor talk too wise:"
+print(verse, "\n")
+
+print("Verse has a length of {} characters.".format(len(verse))) # Verse has a length of 362 characters.
+print("The first occurence of the word 'and' occurs at the {}th index.".format(verse.find('and'))) # The first occurence of the word 'and' occurs at the 65th index.
+print("The last occurence of the word 'you' occurs at the {}th index.".format(verse.rfind('you'))) # The last occurence of the word 'you' occurs at the 186th index.
+print("The word 'you' occurs {} times in the verse.".format(verse.count('you'))) # The word 'you' occurs 8 times in the verse.
