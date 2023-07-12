39404f14e302b2256cd36931db46d9e3fa13cd86
Mohammad Imteyaz <imteyazm745@gmail.com>
2023-07-12 16:22:06 +0530

    added +,-,*,/

--- Projects/Mini_Projects/calculator.py
+++ Projects/Mini_Projects/calculator.py
@@ -1,3 +1,19 @@
-x = input("Enter first Number: ")
-y = input("Enter second Number: ")
-z = print(x+y)
+a = input("Enter first Number: ")
+b = input("Enter second Number: ")
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+
+operation = input('+ for addition, - for subtraction. * for multiplication, / for division')
+if operation == '+':
+    print(add)
+
+elif operation == '-':
+    print(sub)
+
+elif operation == '*':
+    print(mul)
+
+elif operation == '/':
+    print(div)
