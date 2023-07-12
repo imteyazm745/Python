9be7c05de37fc801a8eab62e1cd45071dcfda453
Mohammad Imteyaz <imteyazm745@gmail.com>
2023-07-12 16:40:28 +0530

    else stmt added

--- Projects/Mini_Projects/calculator.py
+++ Projects/Mini_Projects/calculator.py
@@ -5,7 +5,7 @@ sub = a-b
 mul = a*b
 div = a/b
 
-operation = input('+ for addition, - for subtraction. * for multiplication, / for division')
+operation = input('+ for addition, - for subtraction. * for multiplication, / for division : ')
 if operation == '+':
     print(add)
 
@@ -17,3 +17,6 @@ elif operation == '*':
 
 elif operation == '/':
     print(div)
+
+else:
+    print('Invalid operation type (+, -, *, /) one at a time.')
