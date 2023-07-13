0a1faf2c0b6a16c3a36014442e1c76f854055eeb
Mohammad Imteyaz <imteyazm745@gmail.com>
2023-07-13 10:49:06 +0530

    functions added

---
+++ Projects/Mini_Projects/calculator_using_function.py
@@ -0,0 +1,25 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def power(x, y):
+    return x ** y
+
+def modulo(x, y):
+    return x % y
+
+def negate(x):
+    return -x
+
+def abs(x):
+    return abs(x)
+
+
