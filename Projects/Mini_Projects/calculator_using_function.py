4e097dd433e468649dce6b00d90819fc8b78b046
Mohammad Imteyaz <imteyazm745@gmail.com>
2023-07-13 11:16:24 +0530

    choice selection with if else statement

--- Projects/Mini_Projects/calculator_using_function.py
+++ Projects/Mini_Projects/calculator_using_function.py
@@ -24,3 +24,28 @@ def abs(x):
 
 print("Select operation.")
 print('1. Addition\n 2. Subtraction\n 3. multiplication\n 4.Division\n')
+
+choice = int(input('Enter your choice(1/2/3/4)'))
+if choice in (1, 2, 3, 4):
+    try:
+        num1 = float(input('Enter first number'))
+        num2 = float(input('Enter second number'))
+    except ValueError:
+        print('Invalid input. Please enter a number')
+        exit()
+
+    if choice == 1:
+        print(num1, "+", num2, "=", add(num1, num2))
+
+    elif choice == 2:
+        print(num1, "-", num2, "=", subtract(num1, num2))
+
+    elif choice == 3:
+        print(num1, "*", num2, "=", multiply(num1, num2))
+
+    elif choice == 4:
+        print(num1, "/", num2, "=", divide(num1, num2))
+
+    else:
+        print('Invalid input. Please select correct operation')
+        exit()
