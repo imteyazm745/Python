8efb1b83da96aa802dd7296b3594403b751f471e
Mohammad Imteyaz <imteyazm745@gmail.com>
2023-07-13 11:53:31 +0530

    basic level completed

--- Projects/Mini_Projects/calculator_using_function.py
+++ Projects/Mini_Projects/calculator_using_function.py
@@ -10,42 +10,37 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
-def power(x, y):
-    return x ** y
-
-def modulo(x, y):
-    return x % y
-
-def negate(x):
-    return -x
-
-def abs(x):
-    return abs(x)
-
 print("Select operation.")
-print('1. Addition\n2. Subtraction\n3. multiplication\n4.Division\n')
+print('1. Addition\n2. Subtraction\n3. multiplication\n4. Division\n')
 
-choice = int(input('Enter your choice(1/2/3/4) : '))
-if choice in (1, 2, 3, 4):
-    try:
-        num1 = float(input('Enter first number : '))
-        num2 = float(input('Enter second number : '))
-    except ValueError:
-        print('Invalid input. Please enter a number')
-        exit()
+while True:
+    # take input from the user
+    choice = int(input('Enter your choice(1/2/3/4) : '))
+    
+    # check if choice is one of the four options
+    if choice in (1, 2, 3, 4):
+        try:
+            num1 = float(input('Enter first number : '))
+            num2 = float(input('Enter second number : '))
+        except ValueError:
+            print('Invalid input. Please enter a number')
+            continue
 
-    if choice == 1:
-        print(num1, "+", num2, "=", add(num1, num2))
+        if choice == 1:
+            print(num1, "+", num2, "=", add(num1, num2))
 
-    elif choice == 2:
-        print(num1, "-", num2, "=", subtract(num1, num2))
+        elif choice == 2:
+            print(num1, "-", num2, "=", subtract(num1, num2))
 
-    elif choice == 3:
-        print(num1, "*", num2, "=", multiply(num1, num2))
+        elif choice == 3:
+            print(num1, "*", num2, "=", multiply(num1, num2))
 
-    elif choice == 4:
-        print(num1, "/", num2, "=", divide(num1, num2))
+        elif choice == 4:
+            print(num1, "/", num2, "=", divide(num1, num2))
 
+        another_calculation = input("Do You want to calculate again? (y/n) : ")
+        if another_calculation == 'n':
+          break
+            
     else:
         print('Invalid input. Please select correct operation')
-        exit()
