3acfac750476245345708df4f6715a890f0d5308
Mohammad Imteyaz <imteyazm745@gmail.com>
2023-07-15 10:50:51 +0530

    not yet commpleted

--- Projects/hangman.py
+++ Projects/hangman.py
@@ -11,4 +11,25 @@ word = random.choice(someWords)
 
 if __name__ == '__main__':
     print('Guess the word! Hint: word is a name of a fruit ')
-    
+
+    for i  in word:
+        print('_', end=' ')
+
+    print()
+
+    playing = True
+
+    letterGuessed = ''
+    chances = len(word)
+    correct = 0
+    flag = 0
+    try: 
+        while (chances != 0) and flag == 0: 
+            print()
+            chances -= 1
+
+            try:
+                guess = str(input('Enger a letter to guess: '))
+            except:
+                print('Enter only a character')
+                continue
