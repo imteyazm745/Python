68244c391258863821323bd22b61292ff1707af5
Mohammad Imteyaz <imteyazm745@gmail.com>
2023-07-15 13:15:45 +0530

    Now completed and getting exact output

--- Projects/hangman.py
+++ Projects/hangman.py
@@ -1,5 +1,8 @@
-import random 
+import random
 from collections import Counter
+
+name = input("Enter your Name: ")
+print('Good Luck, {}!'.format(name))
 
 someWords = '''apple banana mango strawberry
 orange grape pineapple apricot lemon coconut watermelon
@@ -10,9 +13,9 @@ someWords = someWords.split(' ')
 word = random.choice(someWords)
 
 if __name__ == '__main__':
-    print('Guess the word! Hint: word is a name of a fruit ')
+    print('Guess the word! Hint: word is a name of a fruit')
 
-    for i  in word:
+    for _ in word:
         print('_', end=' ')
 
     print()
@@ -23,18 +26,18 @@ if __name__ == '__main__':
     chances = len(word)
     correct = 0
     flag = 0
-    try: 
-        while (chances != 0) and flag == 0: 
+    try:
+        while chances != 0 and flag == 0:
             print()
             chances -= 1
 
             try:
-                guess = str(input('Enger a letter to guess: '))
+                guess = input('Enter a letter to guess: ')
             except:
                 print('Enter only a character')
                 continue
 
-            #Validation of the guess
+            # Validation of the guess
             if not guess.isalpha():
                 print('Enter only a letter')
                 continue
@@ -45,30 +48,29 @@ if __name__ == '__main__':
                 print('You have already guessed this letter')
                 continue
 
-            #if letter is guessed correctly
+            # If letter is guessed correctly
             if guess in word:
                 k = word.count(guess)
                 for _ in range(k):
                     letterGuessed += guess
 
             for char in word:
-                if char in letterGuessed and (Counter(letterGuessed) != Counter(word)):
+                if char in letterGuessed and Counter(letterGuessed) != Counter(word):
                     print(char, end=' ')
-
-                elif (Counter(letterGuessed)== Counter(word)):
-                    print("The word is:", end=' ')
+                elif Counter(letterGuessed) == Counter(word):
+                    print("\nThe word is:", end=' ')
                     print(word)
                     flag = 1
-                    print('Coongratulations, you won!')
+                    print('Congratulations {}, you won!'.format(name))
                     break
-                    break
-                else:
-                    print('_', end=' ')
+            else:
+                print('_', end=' ')
 
-            if chances and letterGuessed == 0 and (Counter(letterGuessed) != Counter(word)):
-                print()
-                print('You lost! Try again.')
-                print('The word was {}'.format(word))
+        # If user has used all of their chances
+        if chances == 0 and Counter(letterGuessed) != Counter(word):
+            print()
+            print('Hey {} You lost! Try again.'.format(name))
+            print('The word was {}'.format(word))
 
     except KeyboardInterrupt:
         print()
