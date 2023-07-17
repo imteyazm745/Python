12036b88223d0c74386de0d6e2679b2b2d48f0ca
Mohammad Imteyaz <imteyazm745@gmail.com>
2023-07-17 12:08:16 +0530

    mode program is completed

--- Projects/mean_median_mode.py
+++ Projects/mean_median_mode.py
@@ -17,3 +17,19 @@ else:
     median = list2[len(list2)//2]
 
 print('Median of {} is {}'.format(list2,median))
+
+#Mode 
+
+list3 = [14, 18, 22, 24, 10, 28, 23, 25, 26, 22]
+frequency = {}
+mode = None
+for i in list3:
+    frequency.setdefault(i, 0)
+    frequency[i] += 1
+
+frequent = max(frequency.values())
+for i, j in frequency.items():
+    if j == frequent:
+        mode = i
+
+print('Mode of {} is {}'.format(list3, mode))
