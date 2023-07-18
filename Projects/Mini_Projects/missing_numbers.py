b3eda6cc3949555422f7cf878cd8f46b60944df0
Mohammad Imteyaz <imteyazm745@gmail.com>
2023-07-18 12:09:55 +0530

    it will iterate over the input array

--- Projects/Mini_Projects/missing_numbers.py
+++ Projects/Mini_Projects/missing_numbers.py
@@ -2,3 +2,10 @@ def findMissingNumbers(n):
     numbers = set(n)
     length = len(n)
     output = []
+    for i in range(1, n[-1]):
+        if i not in numbers:
+            output.append(i)
+    return output
+
+listOfNumbers = [1, 2, 3, 5, 6, 7, 8, 9, 11, 12, 13, 14, 16, 17, 18]
+print(findMissingNumbers(listOfNumbers))
