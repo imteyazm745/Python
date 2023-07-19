2438a7f0d7a7e760a268111515d2967fc17785c6
Mohammad Imteyaz <imteyazm745@gmail.com>
2023-07-19 11:45:10 +0530

    lower method is applied

--- Projects/Mini_Projects/rock_paper_scissors.py
+++ Projects/Mini_Projects/rock_paper_scissors.py
@@ -1,13 +1,13 @@
 import random
 
-player1 = input("Select Rock, Paper, or Scissor :")
+player1 = input("Select Rock, Paper, or Scissor : ").lower()
 player2 = random.choice(["Rock", "Paper", "Scissor"])
 
-if player1 == "Rock" and player2 == "Paper":
+if player1 == "rock" and player2 == "paper":
     print("Player 2 Won")
-elif player1 == "Paper" and player2 == "Scissor":
+elif player1 == "paper" and player2 == "scissor":
     print("Player 2 Won")
-elif player1 == "Scissor" and player2 == "Rock":
+elif player1 == "scissor" and player2 == "rock":
     print("Player 2 Won")
 elif player1 == player2:
     print('Draw')
