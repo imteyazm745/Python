fed28a478e218422637963621555f2c4cd693f1c
Mohammad Imteyaz <imteyazm745@gmail.com>
2023-07-19 12:05:23 +0530

    print emoji program

---
+++ Projects/Mini_Projects/print_emoji.py
@@ -0,0 +1,6 @@
+# first we need to install emoji Library using "pip install emoji"
+
+import emoji
+
+print(emoji.emojize("I love reading books:books:"))
+print(emoji.emojize("Some people have a very sensitive heart:red_heart:, please be kind with them.:hibiscus:"))
