6e636289c556350b9bcc5192c8277402fc769ad2
Mohammad Imteyaz <imteyazm745@gmail.com>
2023-07-21 10:44:02 +0530

    modified

--- Projects/Mini_Projects/number_game.py
+++ Projects/Mini_Projects/number_game.py
@@ -22,3 +22,36 @@ def check(xyz):
         i += 1
 
     return
+
+#starts the game
+def start1():
+    xyz = []
+    last = 0
+    while True:
+        print("Enter 'F' to take the first chance.")
+        print("Enter 'S' to take the second chance.")
+        chance = input('>')
+
+        #player takes the first chance
+        if chance == 'F':
+            while True:
+                if last == 20:
+                    lose1()
+                else:
+                    print("\nYour Turn.")
+                    print("\nHow many numbers do you wish to enter ? ")
+                    inp = int(input('> '))
+
+                    if inp > 0 and inp <=3:
+                        comp = 4 - inp
+                    else:
+                        print("Wrong input. You are disqualified from the game.")
+                        lose1()
+
+                    i , j = 1, 1
+
+                    print("Now enter the values ")
+                    while i <= inp:
+                        a = input('> ')
+                        a = int(a)
+                        i += 1
